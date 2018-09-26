1c1e857bbba3b3a4263489e04815d8e40853fe9f
Owen J Sullivan <multiojuice@gmail.com>
2018-09-26 00:07:55 -0700

    Adding handlers for web socket messages

--- src/pages/uploadPage/UploadPage.jsx
+++ src/pages/uploadPage/UploadPage.jsx
@@ -7,14 +7,35 @@ class UploadPage extends Component {
 
     window.WebSocket = window.WebSocket || window.MozWebSocket;
     this.connection = new WebSocket('ws://127.0.0.1:1337');
-    this.connection.onopen = function () {
-      // first we want users to enter their names
-      console.warn('connected')
-    };
+    this.connection.onopen = this.handleOnOpen;
+    this.connection.onerror = this.handleOnError;
+    this.connection.onmessage = this.handleOnMessage;
+  }
+
+  handleOnOpen = () => {
+    console.warn('Connected!');
+  }
+
+  handleOnError = (error) => {
+    console.error("There was a connection error");
+  }
+
+  handleOnMessage = (message) => {
+    console.warn('Recieved a message: ', message);
+  }
+
+  sendMessage = () => {
+    console.log('sent message')
+    this.connection.send('Hi there matie');
   }
 
   render() {
-    return <Title>Hey this is the upload page</Title>
+    return (
+      <div>
+        <Title>Hey this is the upload page</Title>
+        <button onClick={this.sendMessage}>CLICK HERE TO SEND A MESSAGE</button>
+      </div>
+    );
   }
 }
 
