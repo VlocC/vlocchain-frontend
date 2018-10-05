046d32f80313ada80e010e2a6db339ca2dff6fd6
Owen J Sullivan <multiojuice@gmail.com>
2018-10-05 10:50:24 -0700

    Moved when we call each funciton. Waits for all objects to be open before requesting video info

--- src/pages/videoPage/VideoPage.jsx
+++ src/pages/videoPage/VideoPage.jsx
@@ -15,11 +15,11 @@ class VideoPage extends Component {
   constructor(props) {
     super(props);
     this.state = {infoLoading: true};
-
     window.WebSocket = window.WebSocket || window.MozWebSocket;
     this.connection = null;
-    this.myMediaSource = new MediaSource();
-    this.streamUrl = URL.createObjectURL(this.myMediaSource);
+    this.myMediaSource = null;
+    this.streamUrl = null;
+    console.warn("Is mp4 supported", MediaSource.isTypeSupported('video/mp4; codecs="avc1.42E01E, mp4a.40.2"'))
   }
 
   componentDidMount() {
@@ -34,13 +34,18 @@ class VideoPage extends Component {
       this.setupWebSocket('ws://127.0.0.1:1337');
       //this.getVideoSource(id);
 
-      this.setState({...response, infoLoading : false})
+      this.setState({...response})
     })
     .catch(error => console.error('Error:', error));
   }
 
   getVideoSource = (id) => {
     this.connection.send(`START:${id}`);
+  }
+
+  handleSourceOpen = () => {
+    console.warn('In handle Source Open')
+    this.connection.send(`START:${this.props.match.params.videoId}`);
   }
 
   setupWebSocket = (url) => {
@@ -56,7 +61,12 @@ class VideoPage extends Component {
 
   handleOnOpen = () => {
     console.warn('Connected!');
-    this.connection.send(`START:${this.props.match.params.videoId}`);
+    this.myMediaSource = new MediaSource();
+    console.warn(this.myMediaSource)
+    this.streamUrl = URL.createObjectURL(this.myMediaSource);
+    this.myMediaSource.addEventListener('sourceopen', this.handleSourceOpen);
+    this.setState({infoLoading: false});
+
   }
 
   handleOnError = (error) => {
@@ -67,7 +77,7 @@ class VideoPage extends Component {
     console.log(this.streamUrl)
     console.log(message.data)
     const videoSourceBuffer = this.myMediaSource
-    .addSourceBuffer('video/webm; codecs="vorbis,vp8"');
+    .addSourceBuffer('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
 
     videoSourceBuffer.addEventListener('updateend', () => {
       this.myMediaSource.endOfStream();
@@ -108,83 +118,3 @@ class VideoPage extends Component {
 }
 
 export default VideoPage;
-
-
-// var FILE = 'test.webm';
-// var NUM_CHUNKS = 5;
-// var video = document.querySelector('video');
-//
-// window.MediaSource = window.MediaSource || window.WebKitMediaSource;
-// if (!!!window.MediaSource) {
-//   alert('MediaSource API is not available');
-// }
-//
-// var mediaSource = new MediaSource();
-//
-// document.querySelector('[data-num-chunks]').textContent = NUM_CHUNKS;
-//
-// video.src = window.URL.createObjectURL(mediaSource);
-//
-// function callback(e) {
-//   var sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vorbis,vp8"');
-//
-//   logger.log('mediaSource readyState: ' + this.readyState);
-//
-//   GET(FILE, function(uInt8Array) {
-//     var file = new Blob([uInt8Array], {type: 'video/webm'});
-//     var chunkSize = Math.ceil(file.size / NUM_CHUNKS);
-//
-//     logger.log('num chunks:' + NUM_CHUNKS);
-//     logger.log('chunkSize:' + chunkSize + ', totalSize:' + file.size);
-//
-//     // Slice the video into NUM_CHUNKS and append each to the media element.
-//     var i = 0;
-//
-//     (function readChunk_(i) {
-//       var reader = new FileReader();
-//
-//       // Reads aren't guaranteed to finish in the same order they're started in,
-//       // so we need to read + append the next chunk after the previous reader
-//       // is done (onload is fired).
-//       reader.onload = function(e) {
-//         sourceBuffer.appendBuffer(new Uint8Array(e.target.result));
-//         logger.log('appending chunk:' + i);
-//         if (i == NUM_CHUNKS - 1) {
-//           mediaSource.endOfStream();
-//         } else {
-//           if (video.paused) {
-//             video.play(); // Start playing after 1st chunk is appended.
-//           }
-//           readChunk_(++i);
-//         }
-//       };
-//
-//       var startByte = chunkSize * i;
-//       var chunk = file.slice(startByte, startByte + chunkSize);
-//
-//       reader.readAsArrayBuffer(chunk);
-//     })(i);  // Start the recursive call by self calling.
-//   });
-// }
-//
-// mediaSource.addEventListener('sourceopen', callback, false);
-// mediaSource.addEventListener('webkitsourceopen', callback, false);
-//
-// mediaSource.addEventListener('webkitsourceended', function(e) {
-//   logger.log('mediaSource readyState: ' + this.readyState);
-// }, false);
-//
-// function GET(url, callback) {
-//   var xhr = new XMLHttpRequest();
-//   xhr.open('GET', url, true);
-//   xhr.responseType = 'arraybuffer';
-//   xhr.send();
-//
-//   xhr.onload = function(e) {
-//     if (xhr.status != 200) {
-//       alert("Unexpected status code " + xhr.status + " for " + url);
-//       return false;
-//     }
-//     callback(new Uint8Array(xhr.response));
-//   };
-// }
