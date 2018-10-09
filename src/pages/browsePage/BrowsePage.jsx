a2b0142b89f7d9e45ea19ad5ae4995a3e40647fb
Owen J Sullivan <multiojuice@gmail.com>
2018-10-08 19:29:55 -0700

    Adding real design and components to the browse page

--- src/pages/browsePage/BrowsePage.jsx
+++ src/pages/browsePage/BrowsePage.jsx
@@ -1,15 +1,116 @@
 import React, {Component} from 'react';
-import { Title } from './styledComponents.js';
+import {
+  Title,
+  BrowseWrapper,
+  BrowseGrid,
+  BrowseItem,
+  BrowseItemAuthor,
+  BrowseItemThumbnail,
+  BrowseItemTitle
+} from './styledComponents.js';
 
+const ITEMS = [
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'red'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'blue'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'green'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'purple'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'yellow'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'navy'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'white'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'red'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'blue'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'green'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'purple'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'yellow'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'navy'
+  },
+  {
+    author: 'Owen Sullivan',
+    title: 'An example',
+    color: 'white'
+  },
+]
 
 class BrowsePage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      items: ITEMS
+    }
+  }
+
+  renderVideos() {
+    return this.state.items.map((item) => {
+      return (
+        <BrowseItem>
+          <BrowseItemThumbnail style={{backgroundColor: item.color}} />
+          <BrowseItemTitle>{item.title}</BrowseItemTitle>
+          <BrowseItemAuthor>{item.author}</BrowseItemAuthor>
+        </BrowseItem>
+      );
+    })
   }
 
   render() {
-    return (<Title>Hey this is the home page</Title>
-  )
+    return (
+      <BrowseWrapper>
+        <Title>These vloccs are hot :fire:</Title>
+        <BrowseGrid>
+          {this.renderVideos()}
+        </BrowseGrid>
+      </BrowseWrapper>
+    )
   }
 }
 
--- src/pages/browsePage/styledComponents.js
+++ src/pages/browsePage/styledComponents.js
@@ -1,6 +1,43 @@
 import styled from 'styled-components';
 
+export const BrowseWrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
 export const Title = styled.div`
   font-size: 70px;
-  color: blue;
+  color: white;
+`;
+
+export const BrowseGrid = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const BrowseItem = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  margin: 30px 30px 30px 30px;
+`;
+
+export const BrowseItemThumbnail = styled.div`
+  height: 200px;
+  width: 300px;
+  background-color: red;
+`;
+
+export const BrowseItemAuthor = styled.div`
+  color: grey;
+`;
+
+export const BrowseItemTitle = styled.div`
+  color: white;
+  margin-top: 10px;
 `;
